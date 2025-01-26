a5c5e40ad82a8e2a9f1b55379a26f0e81be20ef3
JoelMndz <luisjo3lml@gmail.com>
2025-01-26 10:44:53 -0500

    Feat Agregar script de backups

---
+++ backupPostgres.sh
@@ -0,0 +1,4 @@
+echo "Hola munndo"
+# docker exec postgres mkdir -p /var/lib/postgresql/data/backups
+# docker exec postgres pg_dump -U postgres -h localhost -p 5432 -F c -b -v -f /var/lib/postgresql/data/backups/autenticacion_dev_$(date +\%Y\%m\%d_\%H\%M\%S).dump autenticacion_dev
+# docker exec postgres pg_dump -U postgres -h localhost -p 5432 -F c -b -v -f /var/lib/postgresql/data/backups/dominios_dev_$(date +\%Y\%m\%d_\%H\%M\%S).dump dominios_dev
